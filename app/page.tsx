873b2824cfb033b93e9898fc74e74d6231be9f72
Winchesteryyd <demirciyusufyahya72@gmail.com>
2025-06-19 21:40:28 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+  ```tsx
+   export default function Home() {
+     return (
+       <main style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+         <h1>GhostLedgers</h1>
+       </main>
+     )
+   }
+   ```
